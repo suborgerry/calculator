f06b6fec2398beea31f8c4699850964384808975
borgerry <gerryborodin@gmail.com>
2023-02-06 18:46:14 +0200

    Handle provder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components'
 
 function App() {
+
+  const [storageCount, setStorageCount] = useState(0);
+  const [transferCount, setTransferCount] = useState(0);
+
+  // Style components
+  const Title = styled.h1`
+    font-size: 1em;
+  `;
+
+  const String = styled.div`
+    width: ${props => props.size}px;
+    height: 20px;
+    background: #000
+  `;
+
+  // Components
+  const Provider = (props) => {
+    let storage = storageCount * props.storageCoeff;
+    let transfer = transferCount * props.transferCoeff;
+
+    let price = (storage + transfer).toFixed(2);
+    let count = Math.round(price * 10);
+
+    return(
+      <>
+        <String size={count}></String><div>${price}</div>
+      </>
+    )
+  }
+
+  // Handle functions
+  const handleStorage = e => {
+    let count = e.target.value;
+    setStorageCount(count);
+  }
+
+  const handleTransfer = e => {
+    let count = e.target.value;
+    setTransferCount(count);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='inputs'>
+        <div>
+          <input type="range" name="cowbell" min="0" defaultValue={0} max="1000" step="1" onChange={handleStorage}/>
+          <Title>Storage: {storageCount}GB</Title>
+        </div>
+        <div>
+          <input type="range" name="cowbell" min="0" defaultValue={0} max="1000" step="1" onChange={handleTransfer}/>
+          <Title>Transfer: {transferCount}GB</Title>
+        </div>
+      </div>
+      <div className='strings'>
+        <div>
+          <p>Backblaze</p>
+          <Provider storageCoeff="0.005" transferCoeff="0.01"/>
+        </div>
+        <div>
+          <p>Vultr</p>
+          <Provider storageCoeff="0.01" transferCoeff="0.01"/>
+        </div>
+      </div>
     </div>
   );
 }
